72821ba63e112341255e204ad38e049e302b1e99
LucaGunardi <148530963+LucaGunardi@users.noreply.github.com>
2024-01-03 20:44:29 +0100

    tm 20

--- JS/H2/H2O19.js
+++ JS/H2/H2O19.js
@@ -1,6 +1,24 @@
-var aantalRijenRaster = 6;
-var aantalKolommenRaster = 9;
-var celGrootte;
+var raster = {
+  aantalRijen : 6,
+  aantalKolommen : 6,
+  celGrootte : null,
+
+  berekenCelGrootte() {
+    this.celGrootte = width/this.aantalKolommen;
+  },
+
+  teken() {
+    push();
+    noFill();
+    stroke('grey');
+    for (var rij = 0;rij < this.aantalRijen;rij++) {
+      for (var kolom = 0;kolom < this.aantalKolommen;kolom++) {
+      rect(kolom*raster.celGrootte,rij*raster.celGrootte,raster.celGrootte,raster.celGrootte);
+    }
+  }
+  pop();
+  }
+}
 
 var animatie = [];
 var aantalBeeldjes = 6;
@@ -8,6 +26,8 @@ var nummer = 3;
 var frame;
 var xJos = 400;
 var yJos = 300;
+
+
 
 function preload() {
   brug = loadImage("images/backgrounds/dame_op_brug_1800.jpg");
@@ -21,44 +41,32 @@ function setup() {
   canvas = createCanvas(900,600);
   canvas.parent('processing');
   frameRate(10);
-  celGrootte = width / aantalKolommenRaster;
+  raster.berekenCelGrootte();
 }
 
 function draw() {
   background(brug);
-  tekenRaster();
+  raster.teken();
 
   if (keyIsDown(LEFT_ARROW)) {
-    xJos -= celGrootte;
+    xJos -= raster.celGrootte;
     nummer = 2;
   }
   if (keyIsDown(RIGHT_ARROW)) {
-    xJos+=celGrootte;
+    xJos+=raster.celGrootte;
     nummer=1;
   }
   if (keyIsDown(UP_ARROW)) {
-    yJos -= celGrootte;
+    yJos -= raster.celGrootte;
     nummer = 4;
   }
   if (keyIsDown(DOWN_ARROW)) {
-    yJos += celGrootte;
+    yJos += raster.celGrootte;
     nummer = 5;
   }
   
-  xJos = constrain(xJos,0,width - celGrootte);
-  yJos = constrain(yJos,0,height - celGrootte);
+  xJos = constrain(xJos,0,width - raster.celGrootte);
+  yJos = constrain(yJos,0,height - raster.celGrootte);
   
-  image(animatie[nummer],xJos,yJos,celGrootte,celGrootte);
-}
-
-function tekenRaster() {
-  push();
-  noFill();
-  stroke('grey');
-  for (var rij = 0;rij < aantalRijenRaster;rij++) {
-    for (var kolom = 0;kolom < aantalKolommenRaster;kolom++) {
-      rect(kolom*celGrootte,rij*celGrootte,celGrootte,celGrootte);
-    }
-  }
-  pop();
+  image(animatie[nummer],xJos,yJos,raster.celGrootte,raster.celGrootte);
 }
